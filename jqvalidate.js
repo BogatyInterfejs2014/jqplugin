e110f4c9303880ec02aae06582b23695c4ada7fc
Robert Tomczak <r0b3rt2@wp.pl>
2014-05-05 21:52:14 +0200

    Correction after jshinting

--- jqvalidate.js
+++ jqvalidate.js
@@ -18,80 +18,80 @@
 			passcompl: function(obj) {
 				var password = obj.val();
 				var passScore = 0;
-				var upperchar = /[A-Z]/g
-				var lowerchar = /[a-z]/g
-				var numberpass = /[0-9]/g
-				var specialchars = /[!@#$%^&*?_~]/g
+				var upperchar = /[A-Z]/g;
+				var lowerchar = /[a-z]/g;
+				var numberpass = /[0-9]/g;
+				var specialchars = /[!@#$%^&*?_~]/g;
 				var checkisnull = function(str){
 					var temp = password.match(str);
-					if(temp == null){
+					if(temp === null){
 						temp = 0;
 					}else{
 						temp = password.match(str).length;
 					}
 					return temp;
-				}
+				};
 				//Sprawdzanie dlugosci
 				if (password.length < 5){
-			        passScore += 5;
-			    }
-			    else if (password.length > 4 && password.length < 8){
-			        passScore += 10;
-			    }
-			    else if (password.length > 7){
-			        passScore += 25;
-			    }
+					passScore += 5;
+				}
+				else if (password.length > 4 && password.length < 8){
+					passScore += 10;
+				}
+				else if (password.length > 7){
+					passScore += 25;
+				}
 
-			    var uppercount = checkisnull(upperchar);
-			    var lowercount = checkisnull(lowerchar);
+				var uppercount = checkisnull(upperchar);
+				var lowercount = checkisnull(lowerchar);
 
-			    //Sprawdzanie czy haslo posiada tylko male litery
-			    if (uppercount == 0 && lowercount != 0){ 
-			        passScore += 10; 
-			    }
-			    //Sprawdzanie czy haslo posiada male i duze litery
-			    else if (uppercount != 0 && lowercount != 0){ 
-			        passScore += 20; 
-			    }
-			    //Sprawdzanie czy haslo zawiera liczby
-			    var numbercount = checkisnull(numberpass);
-			    if (numbercount == 1){
-			        passScore += 10;
-			    }
-			    if (numbercount >= 3){
-			        passScore += 20;
-			    }
-			    //Sprawdzanie czy haslo posiada znaki specjalne
-			    var charactercount = checkisnull(specialchars);
-			    if (charactercount == 1){
-			        passScore += 10;
-			    }   
-			    if (charactercount > 1){
-			        passScore += 25;
-			    }
-			    //Sprawdzanie czy haslo posiada liczby, duze i male litery
-			    if (numbercount != 0 && uppercount != 0 && lowercount != 0){
-			        passScore += 2;
-			    }
-				//Sprawdzanie czy haslo posiada liczby, duze i male litery oraz znaki specjalne			    
-				if (numbercount != 0 && uppercount != 0 && lowercount != 0 && charactercount != 0)
-			    {
-			        passScore += 5;
-			    }
-			    //Zwracanie sily wprowadzanego hasla
-			    if (passScore >= 80){
-			        var strText = "Very Strong";
-			    }else if (passScore >= 60){
-			        var strText = "Strong";
-			    }else if (passScore >= 40){
-			        var strText = "Average";
-			    }else if (passScore >= 20){
-			        var strText = "Weak";
-			    }else{
-			        var strText = "Very Weak";
-			    }
+				//Sprawdzanie czy haslo posiada tylko male litery
+				if (uppercount === 0 && lowercount !== 0){ 
+					passScore += 10; 
+				}
+				//Sprawdzanie czy haslo posiada male i duze litery
+				else if (uppercount !== 0 && lowercount !== 0){ 
+					passScore += 20; 
+				}
+				//Sprawdzanie czy haslo zawiera liczby
+				var numbercount = checkisnull(numberpass);
+				if (numbercount === 1){
+					passScore += 10;
+				}
+				if (numbercount >= 3){
+					passScore += 20;
+				}
+				//Sprawdzanie czy haslo posiada znaki specjalne
+				var charactercount = checkisnull(specialchars);
+				if (charactercount === 1){
+					passScore += 10;
+				}   
+				if (charactercount > 1){
+					passScore += 25;
+				}
+				//Sprawdzanie czy haslo posiada liczby, duze i male litery
+				if (numbercount !== 0 && uppercount !== 0 && lowercount !== 0){
+					passScore += 2;
+				}
+				//Sprawdzanie czy haslo posiada liczby, duze i male litery oraz znaki specjalne
+				if (numbercount !== 0 && uppercount !== 0 && lowercount !== 0 && charactercount !== 0){
+					passScore += 5;
+				}
+				//Zwracanie sily wprowadzanego hasla
+				var strText = "";
+				if (passScore >= 80){
+					strText = "Very Strong";
+				}else if (passScore >= 60){
+					strText = "Strong";
+				}else if (passScore >= 40){
+					strText = "Average";
+				}else if (passScore >= 20){
+					strText = "Weak";
+				}else{
+					strText = "Very Weak";
+				}
 
-			    return strText;
+				return strText;
 
 			}
 
